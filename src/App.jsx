f6bdc963285f5cbcf943969d78ad81885466b543
Juanco <210373@utxicotepec.edu.mx>
2024-09-18 11:02:59 -0600

    Instala y configura react-router-dom para la navegacion y crea las primeras paginas de la aplicacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route} from 'react-router-dom'
+
+import Home from './Pages/Home'
+import NotFound from './Pages/NotFound'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hola bienvenido</h1>
+      <Routes>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//Configuracion de React-Router-Dom
+import { BrowserRouter as Router } from 'react-router-dom'
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Router>
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  </Router>
 )
