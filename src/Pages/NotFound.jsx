504329fc9fe40d7e37c28673bfeed7defbcc44d0
Juanco <juanffo874@gmail.com>
2024-09-23 20:33:48 -0600

    Añade estilos a la pagina de no encontrado

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>Pagina no entrada</h1>
+    <div className='flex justify-center items-center'>
+        <div className='w-1/4'>
+            <h1 className='font-bold text-3xl text-center mt-10'>Oops! Página no encontrada</h1>
+            <p className='text-center text-gray-500 mt-4'>Lo sentimos la página que estas buscando no existe.</p>
+            <p className='text-center text-gray-500'>Vuelve a la pagina principal y continua explorando.</p>
+            <div className='mt-6 flex justify-center'>
+                <Link to='/dashboard' className='bg-gray-800 text-white py-2 px-6 rounded-lg hover:bg-slate-700'>Ir a la página principal</Link>
+            </div>
+        </div>
     </div>
   )
 }
