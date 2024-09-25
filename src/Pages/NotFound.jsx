8f5531eeb2a856b2496f33f9cd54b73e159a28aa
Juanco <juanffo874@gmail.com>
2024-09-24 22:41:04 -0600

    Corrige el link del dashboard

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -9,7 +9,7 @@ const NotFound = () => {
             <p className='text-center text-gray-500 mt-4'>Lo sentimos la página que estas buscando no existe.</p>
             <p className='text-center text-gray-500'>Vuelve a la pagina principal y continua explorando.</p>
             <div className='mt-6 flex justify-center'>
-                <Link to='/dashboard' className='bg-gray-800 text-white py-2 px-6 rounded-lg hover:bg-slate-700'>Ir a la página principal</Link>
+                <Link to='/' className='bg-gray-800 text-white py-2 px-6 rounded-lg hover:bg-slate-700'>Ir a la página principal</Link>
             </div>
         </div>
     </div>
