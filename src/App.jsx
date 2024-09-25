0babf839f54f0b1bb3728f32894e4a433c92bdd7
Juanco <juanffo874@gmail.com>
2024-09-24 18:52:51 -0600

    Corrigiendo la ruta del inicio

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/home' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
         <Route path='*' element={<NotFound/>}/>
       </Routes>
