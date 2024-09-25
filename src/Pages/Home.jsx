4b56d10c7915748274765dba023b68ca8799b7b1
Juanco <juanffo874@gmail.com>
2024-09-24 19:18:46 -0600

    Crea el navbar y lo añade al inicio

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import NavBar from '../components/Home/NavBar'
 
 const Home = () => {
   return (
-    <div>
+    <>
+      <NavBar/>
       <h1>Home</h1>
-    </div>
+    </>
   )
 }
 
---
+++ src/components/Home/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <>
+        <nav className='bg-gray-100 w-full flex justify-between items-center px-6 py-4 relative'>
+            <h1 className='font-bold text-2xl'>Taski</h1>
+            <div className='space-x-4'>
+                <Link className='font-semibold py-2 px-4 rounded-lg hover:bg-slate-200'>Iniciar Sesión</Link>
+                <Link className='bg-gray-900 text-white py-2 px-4 rounded-lg hover:bg-gray-700'>Crear una cuenta</Link>
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default NavBar
