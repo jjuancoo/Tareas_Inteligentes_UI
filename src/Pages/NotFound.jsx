df3fd86b6f081a26d814cf44ceb869d66f91b1b7
Juanco <juanffo874@gmail.com>
2024-09-26 20:40:55 -0600

    Rediseño de la pagina 404

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom'
 const NotFound = () => {
   return (
     <div className='flex justify-center items-center'>
-        <div className='w-1/4'>
-            <h1 className='font-bold text-3xl text-center mt-10'>Oops! Página no encontrada</h1>
+        <div className='w-1/4 mt-10'>
+            <h1 className='text-9xl font-bold text-blue-600 text-center'>404</h1>
+            <h1 className='font-bold text-3xl text-center mt-6'>Oops! Página no encontrada</h1>
             <p className='text-center text-gray-500 mt-4'>Lo sentimos la página que estas buscando no existe.</p>
             <p className='text-center text-gray-500'>Vuelve a la pagina principal y continua explorando.</p>
             <div className='mt-6 flex justify-center'>
