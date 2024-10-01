e8d4c100f8ad36addde267d5a4cbad74e13a2af3
Juanco <juanffo874@gmail.com>
2024-09-30 21:09:02 -0600

    Agrega el modal para salir de la cuenta

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+import Exit from './Dashboard/Exit'
 
 const Sidebar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <>
             <div class="flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800">
@@ -9,18 +14,18 @@ const Sidebar = () => {
                         <ul class="flex flex-col py-4 space-y-1">
                             <li class="px-5">
                                 <div class="flex flex-row items-center h-8">
-                                    <div class="text-sm font-light tracking-wide text-gray-500">Menu</div>
+                                    <div class="text-2xl font-bold tracking-wide text-gray-900">Taski</div>
                                 </div>
                             </li>
                             <li>
-                                <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                                <Link to='/dashboard' class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
                                     <span class="inline-flex justify-center items-center ml-4">
                                         <span class="material-symbols-rounded">
                                             home
                                         </span>
                                     </span>
                                     <span class="ml-2 text-sm tracking-wide truncate">Dashboard</span>
-                                </a>
+                                </Link>
                             </li>
                             <li>
                                 <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
@@ -97,12 +102,20 @@ const Sidebar = () => {
                                 </a>
                             </li>
                             <li>
-                                <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
-                                    <span class="inline-flex justify-center items-center ml-4">
-                                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
-                                    </span>
-                                    <span class="ml-2 text-sm tracking-wide truncate">Logout</span>
-                                </a>
+                                <button 
+                                    class="relative flex flex-row items-center h-11 focus:outline-none w-full hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6"
+                                >
+                                    <Exit
+                                        isOpen={isOpen}
+                                    />                                        
+                                    <Link to="/">
+
+                                        <span class="inline-flex justify-center items-center ml-4">
+                                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
+                                        </span>
+                                        <span class="ml-2 text-sm tracking-wide truncate">Logout</span>
+                                    </Link>
+                                </button>
                             </li>
                         </ul>
                     </div>
