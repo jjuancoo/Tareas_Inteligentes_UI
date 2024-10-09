809fa54c1f335ba1bd1d122ccc0306eeb8c5aa06
Juanco <juanffo874@gmail.com>
2024-10-09 17:56:12 -0600

    Estiliza el footer

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -6,23 +6,23 @@ const Footer = () => {
     <>
     <div className='bg-slate-200'>
         <div className='p-8'>
-            <div className='grid grid-cols-3'>
-                <div className=''>
+            <div className='grid grid-cols-3 p-2'>
+                <div className='space-y-2'>
                     <h2 className='font-bold text-lg'>Taski</h2>
                     <p className=''>Simplificando la gestión de tareas y proyectos para equipos de todos los tamaños</p>
                 </div>
 
-                <div>
-                    <h2 className='font-bold text-lg'>Recursos</h2>
+                <div  className='space-y-2'>
+                    <h2 className='font-bold text-lg'>Productos</h2>
                     <ul className="space-y-2">
-                        <li><Link href="#" className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white">Blog</Link></li>
-                        <li><Link href="#" className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white">Tutoriales</Link></li>
-                        <li><Link href="#" className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white">Documentación</Link></li>
-                        <li><Link href="#" className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white">Comunidad</Link></li>
+                        <li><Link href="#" className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white">Caractersticas</Link></li>
+                        <li><Link href="#" className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white">Integraciones</Link></li>
+                        <li><Link href="#" className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white">Precios</Link></li>
+                        <li><Link href="#" className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white">Hoja de ruta</Link></li>
                     </ul>
                 </div>
 
-                <div>
+                <div className='space-y-2'>
                     <h2 className='font-bold text-lg'>Recursos</h2>
                     <ul className="space-y-2">
                         <li><Link href="#" className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white">Blog</Link></li>
